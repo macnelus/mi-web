4765065e3105472d6e06c28dcd7e4bc2c8418ea0
Macnelus <macnelus@gmail.com>
2020-11-08 14:28:32 +0100

    buiding form

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -3,13 +3,37 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+
 const Contact = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Contact</h1>
+      <main id="contact">
+        <form className="form">
+          <div className="form-control">
+            <label>Name</label>
+            <input id="name" type="text" />
+          </div>
+          <div className="form-control">
+            <label>email</label>
+            <input id="email" type="email" />
+          </div>
+          <div>
+            <label className="box">Message</label>
+            <textarea className="box" id="message" type="textarea" />
+          </div>
+
+          <div className="btn">Send</div>
+          
+        </form>
+
+        
+        <div className="map">
+          <h1>Contact</h1>
+          <p>social media</p>
+        </div>
       </main>
+
       <Footer />
     </>
   );
