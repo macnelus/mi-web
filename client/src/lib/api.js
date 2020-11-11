f4502dbeb6c476ffb39abf0e0a86230cff82d916
Pedro Martin <pataruco@gmail.com>
2020-11-11 11:34:08 +0000

    Draft gapi client

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,14 +1,7 @@
-const { REACT_APP_YOUTUBE_API_KEY } = process.env;
-const YOUTUBE_ITEMS_API = 'https://www.googleapis.com/youtube/v3/channels';
+// import { gapi } from '../App.js';
+// const { REACT_APP_YOUTUBE_API_KEY } = process.env;
+// const YOUTUBE_ITEMS_API = 'https://www.googleapis.com/youtube/v3/channels';
 
 export const getYoutubeApi = async () => {
-  console.log({ REACT_APP_YOUTUBE_API_KEY });
-  const res = await fetch(
-    `${YOUTUBE_ITEMS_API}?part=snippet%2Cstatistics&id=UC-jqp9Jz_9tiGiVVtSiPkmw&key=${REACT_APP_YOUTUBE_API_KEY}`,
-  );
-
-  const data = res.json();
-
-  console.log({ data });
-  return data;
+  console.log('gapi', window.gapi);
 };
