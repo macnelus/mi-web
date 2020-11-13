3d8265f4ee499d9d09d31b5e332763687e070587
Pedro Martin <pataruco@gmail.com>
2020-11-13 09:29:21 +0000

    Render youtube videos 📺

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,9 +1,17 @@
 export const getYoutubeApi = async () => {
   const response = await window.gapi.client.request(
-    'https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&id=UC-jqp9Jz_9tiGiVVtSiPkmw',
+    'https://www.googleapis.com/youtube/v3/search?channelId=UC-jqp9Jz_9tiGiVVtSiPkmw&part=snippet,id&order=date&maxResults=50',
   );
 
-  const data = JSON.parse(response.body);
-  console.log({ data });
-  return data;
+  const { items } = JSON.parse(response.body);
+  const youtubedata = items.map((video) => {
+    const {
+      id: { videoId },
+      snippet: { title, description },
+    } = video;
+
+    return { videoId, title, description };
+  });
+
+  return youtubedata;
 };
