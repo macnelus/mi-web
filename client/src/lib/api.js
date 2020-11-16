2b4dfd84be14f82120b61d989acd082b4fa6883e
Macnelus <macnelus@gmail.com>
2020-11-16 10:17:04 +0100

    fixing bugs css

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,10 +1,11 @@
 import Flickr from 'flickr-sdk';
 
-const { REACT_APP_FLICKR_API_KEY} = process.env;
+const { REACT_APP_FLICKR_API_KEY } = process.env;
+const { REACT_APP_YOUTUBE_API_KEY } = process.env;
 
 export const getYoutubeApi = async () => {
   const response = await window.gapi.client.request(
-    'https://www.googleapis.com/youtube/v3/search?key=AIzaSyADCBlhiFO1UNFaZ07STZd6_FlUPr8S7ck&channelId=UC-jqp9Jz_9tiGiVVtSiPkmw&part=snippet,id&order=date&maxResults=50',
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-jqp9Jz_9tiGiVVtSiPkmw&order=date&type=video&videoSyndicated=true&key=${REACT_APP_YOUTUBE_API_KEY}`,
   );
   const { items } = JSON.parse(response.body);
   const youtubedata = items.map((video) => {
@@ -17,6 +18,8 @@ export const getYoutubeApi = async () => {
   });
   return youtubedata;
 };
+
+//flickr//
 
 const getPhotoData = async ({ photoId }) => {
   const flickr = new Flickr(REACT_APP_FLICKR_API_KEY);
