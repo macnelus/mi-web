be8bcd87730274ddfa8441c01a6fbaa15d06e82a
Macnelus <macnelus@gmail.com>
2020-11-17 21:38:37 +0100

    to deploy

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -5,7 +5,7 @@ const { REACT_APP_YOUTUBE_API_KEY } = process.env;
 
 export const getYoutubeApi = async () => {
   const response = await window.gapi.client.request(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-jqp9Jz_9tiGiVVtSiPkmw&order=id&type=video&videoSyndicated=true&key=${REACT_APP_YOUTUBE_API_KEY}`,
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC-jqp9Jz_9tiGiVVtSiPkmw&order=date&type=video&videoSyndicated=true&key=${REACT_APP_YOUTUBE_API_KEY}`,
   );
   const { items } = JSON.parse(response.body);
   const youtubedata = items.map((video) => {
